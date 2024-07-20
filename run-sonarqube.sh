a8b842eef24bf5eddc9c4dc52c44309b7f50ac59
Gilson Russo <gilsonrusso@outlook.com>
2024-07-19 22:58:11 -0400

    update project

--- run-sonarqube.sh
+++ run-sonarqube.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 set -e
 
-# Levanta os containers do Docker
-docker compose up -d
+# Carregar variáveis do arquivo .env
+export $(grep -v '^#' .env | xargs)
+
+# Levantando os containers do Docker
+# docker compose up
 
 # Aguarda alguns segundos para garantir que o SonarQube esteja pronto
-echo "Aguardando SonarQube inicializar..."
-sleep 60
+echo "Inicializando o SonarQube Scanner..."
+sleep 5
 
-# Executa o SonarQube Scanner e salva o resultado
-RESULT=$(docker run --rm --network=host -e SONAR_HOST_URL=http://localhost:9000 -v $(pwd)/src:/usr/src sonarsource/sonar-scanner-cli \
-  -Dsonar.projectKey=test \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=sqp_495bdac2d9df6c43712e32ed21671c0c99e7d22e || echo "fail")
+# Verificar se a variável RUN_SONARQUBE é verdadeira
+if [ "$RUN_SONARQUBE" = "true" ]; then
 
-# Para os containers do Docker após a execução
-docker compose down
+  # Debug: Imprimir variáveis de ambiente
+  echo "SONARQUBE_HOST_URL=${SONARQUBE_HOST_URL}"
+  echo "SONARQUBE_PROJECT_NAME=${SONARQUBE_PROJECT_NAME}"
+  echo "SONARQUBE_PROJECT_TOKEN=${SONARQUBE_PROJECT_TOKEN}"
+  echo "SRC_DIR=$(pwd)/src"
 
-# Verifica o resultado e sai com um código de erro se falhou
-if [ "$RESULT" = "fail" ]; then
-  echo "SonarQube encontrou problemas no código. Corrija-os antes de fazer o push."
-  exit 1
+  # Executa o SonarQube Scanner e salva o resultado
+  RESULT=$(docker run --rm --network=host -e SONAR_HOST_URL=${SONARQUBE_HOST_URL} -v $(pwd)/src:/usr/src sonarsource/sonar-scanner-cli \
+    -Dsonar.projectKey=${SONARQUBE_PROJECT_NAME} \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=${SONARQUBE_HOST_URL} \
+    -Dsonar.login=${SONARQUBE_PROJECT_TOKEN} \
+    -Dsonar.issuesReport.html.enable=true \
+    -Dsonar.issuesReport.html.location=$(pwd)/sonar-reports/ || echo "fail")
+
+  # Para os containers do Docker após a execução
+  # docker compose down
+
+  # Verifica o resultado e sai com um código de erro se falhou
+  if [ "$RESULT" = "fail" ]; then
+    echo "SonarQube encontrou problemas no código. Corrija-os antes de fazer o push."
+    exit 1
+  fi
+
+  echo "Finalizado o SonarQube Scanner!"
+
+else 
+ echo "RUN_SONARQUBE is not set to true. Skipping SonarQube scan."
 fi
